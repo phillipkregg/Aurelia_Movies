e41d72a96fb5024a4e7833ad6acfe9bd2d316442
phillipkregg <phillipkregg@gmail.com>
2016-06-13 14:43:44 -0500

    first commit

---
+++ movies/wwwroot/app.js
@@ -0,0 +1,15 @@
+import {inject} from "aurelia-framework";
+import {MovieData} from "./movieData";
+
+@inject(MovieData)
+export class App {
+  constructor(movieData) {
+    this.movieData = movieData;
+  }
+
+  activate() {
+    return this.movieData.getAll().then(movies => this.movies = movies);
+
+  }
+
+}
---
+++ movies/wwwroot/movieData.js
@@ -0,0 +1,18 @@
+import {inject} from "aurelia-framework";
+import {HttpClient} from "aurelia-http-client";
+
+let baseUrl = "/movies.json";
+
+@inject(HttpClient)
+export class MovieData {
+
+  constructor(httpClient){
+    this.http = httpClient;
+  }
+
+  getAll() {
+    return this.http.get("/movies.json").then(response => {
+      return response.content;
+    })
+  }
+}
